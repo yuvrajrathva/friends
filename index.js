2a48761864b65832594effdb3c40e7cc6981def6
Yuvraj Rathva <rathva.1@iitj.ac.in>
2023-02-01 02:10:30 +0530

    change in footer

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 // Slides
 let slideIndex = 1;
 showSlides(slideIndex);
-showSlides2(slideIndex);
+
 
 function plusSlides(n) {
   showSlides((slideIndex += n));
 }
-function plusSlides2(n) {
-  showSlides2((slideIndex += n));
+function change(){
+    ssetTimeout(plusSlides(n), 2000);
 }
 
 function currentSlide(n) {
   showSlides((slideIndex = n));
 }
-function currentSlide2(n) {
-  showSlides2((slideIndex = n));
-}
+
 
 function showSlides(n) {
   let i;
@@ -30,25 +28,7 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
-
-
-function showSlides2(n) {
-  let i;
-  let slides = document.getElementsByClassName("programSlides");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
+  
   slides[slideIndex - 1].style.display = "block";
 }
+
