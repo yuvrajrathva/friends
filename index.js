477777aa6ed151ba40d729904f07c0ac118ec500
Yuvraj Rathva <rathva.1@iitj.ac.in>
2023-02-01 01:14:59 +0530

    adding initial files

---
+++ index.js
@@ -0,0 +1,59 @@
+// Slides
+let slideIndex = 1;
+showSlides(slideIndex);
+showSlides2(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+function plusSlides2(n) {
+  showSlides2((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+function currentSlide2(n) {
+  showSlides2((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+
+function showSlides2(n) {
+  let i;
+  let slides = document.getElementsByClassName("programSlides");
+  let dots = document.getElementsByClassName("program-dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
